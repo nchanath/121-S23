b423eedc05f3f8160a0d275ff9d9960c22793342
Chanathip Namprempre <chanathipn@reed.edu>
2023-01-08 19:09:32 -0800

    Site updated: 2023-01-08 19:09:32

---
+++ lecturenotes/hello_calc.py
@@ -0,0 +1,12 @@
+"""Test program"""
+
+from math import sqrt
+
+print("Hello there, everyone!")
+
+print("This is our second Python program.")
+
+print("Did you know that 78687 times 89798 is this?")
+
+x = sqrt(4)
+print(x)
