d747679c1c35568449f66e922e63edf9eb78cd80
Chanathip Namprempre <chanathipn@reed.edu>
2023-01-08 21:27:38 -0800

    Site updated: 2023-01-08 21:27:38

---
+++ lecturenotes/onion-peel-recurs.py
@@ -0,0 +1,10 @@
+def peel_onion(layers_left):
+    if layers_left == 0:
+        print("The onion is peeled!")
+        return 
+    else:
+        print(f"Peeling layer {layers_left}")
+        layers_left -= 1
+        peel_onion(layers_left)
+
+peel_onion(5)
