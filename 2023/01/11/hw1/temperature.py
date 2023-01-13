26524ef1a68a054af6fd9bf7af1c74063c8d3cb3
Chanathip Namprempre <chanathipn@reed.edu>
2023-01-12 19:32:10 -0800

    Site updated: 2023-01-12 19:32:10

---
+++ 2023/01/11/hw1/temperature.py
@@ -0,0 +1,4 @@
+
+c = float(input("Temperature in degrees celsius? "))
+f = (c * 9.0 / 5.0) + 32.0
+print("That is "+str(f)+" in degrees fahrenheit.")
---
+++ 2023/01/11/hw1/test.py
@@ -0,0 +1,9 @@
+x=int((3/2)*2) - 1
+i=1
+while i<13:
+    j=3
+    while j<30:
+        x=x+i*j
+        j=j+4
+    i=i+1
+print(x)
