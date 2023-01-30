c577a63df3a8c6eca798ff2271fa445a1f411caf
Chanathip Namprempre <chanathipn@reed.edu>
2023-01-30 12:33:05 -0800

    Site updated: 2023-01-30 12:33:05

---
+++ lecturenotes/largest-of-three.py
@@ -0,0 +1,9 @@
+a = int(input("Enter a: "))
+b = int(input("Enter b: "))
+c = int(input("Enter c: "))
+if a > b and a > c:
+    print("a is the largest")
+elif b > a and b > c:
+    print("b is the largest")
+else:
+    print("c is the largest")
