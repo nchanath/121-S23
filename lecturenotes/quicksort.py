2badf8b0a0a37ab6df3cc47070c52744c136b8a7
Chanathip Namprempre <chanathipn@reed.edu>
2023-04-26 10:45:40 -0700

    Site updated: 2023-04-26 10:45:40

--- lecturenotes/quicksort.py
+++ lecturenotes/quicksort.py
@@ -2,18 +2,18 @@ def quickSort(someList):
     if len(someList) == 0:
         # It's already sorted! BASE CASE.
         return []
+    elif len(someList) == 1:
+        return [someList[0]]
     else:
         smaller,pivot,larger = partition(someList)
-
-        print(f"pivot = {pivot} \t sorting {smaller} and {larger}")
 
         smallerSorted = quickSort(smaller)
         largerSorted = quickSort(larger)
         return smallerSorted + [pivot] + largerSorted
 
 def partition(someList):
-    smallers = []
     pivot = someList[0] # pick some value from the list
+    smallers = []
     largers = []
     for x in someList[1:]:
         if x <= pivot:
@@ -23,7 +23,7 @@ def partition(someList):
     return smallers, pivot, largers
 
 
-l = [40, 22,60,3,65,87,14,56,30,100,33,9]
+l = [40, 22,60,3,65,87,14]
 print(f"original list: {l}")
 print()
 
